beab0e300f3e3898a81d7504eb4b01cdd8f2322d
Ladislav Prskavec <ladislav.prskavec@oracle.com>
2019-05-06 16:13:17 +0200

    chore: add default params and cleanup formatting

--- src/uploader.js
+++ src/uploader.js
@@ -1,10 +1,10 @@
-const {EventEmitter} = require ('events');
+const { EventEmitter } = require('events');
 
-const aws = require ('aws-sdk');
+const aws = require('aws-sdk');
 
 class Uploader extends EventEmitter {
   // Constructor
-  constructor (
+  constructor(
     {
       accessKey,
       secretKey,
@@ -21,11 +21,11 @@ class Uploader extends EventEmitter {
       service,
       debug,
     },
-    cb
+    cb,
   ) {
-    super ();
+    super();
     this.cb = cb;
-    aws.config.update ({
+    aws.config.update({
       accessKeyId: accessKey,
       secretAccessKey: secretKey,
       sessionToken,
@@ -50,33 +50,31 @@ class Uploader extends EventEmitter {
     this.debug = debug || false;
 
     if (!this.objectParams.Bucket) {
-      throw new Error ('Bucket must be given');
+      throw new Error('Bucket must be given');
     }
 
-    this.upload = new aws.S3.ManagedUpload ({
+    this.upload = new aws.S3.ManagedUpload({
       partSize: 10 * 1024 * 1024,
-      queueSize: 1,
+      queueSize: 4,
       service,
       params,
     });
-    this.upload.minPartSize = 1024 * 1024 * 5;
-    this.upload.queueSize = 4;
     // Progress event
-    this.upload.on ('httpUploadProgress', progress => {
+    this.upload.on('httpUploadProgress', progress => {
       if (this.debug) {
-        return console.log (`${progress.loaded} / ${progress.total}`);
+        return console.log(`${progress.loaded} / ${progress.total}`);
       }
     });
   }
   // Send stream
-  send (callback) {
-    return this.upload.send (function (err, data) {
+  send(callback) {
+    return this.upload.send(function(err, data) {
       if (err) {
-        console.log (err, data);
+        console.log(err, data);
       }
-      return callback (err, data);
+      return callback(err, data);
     });
   }
 }
 
-module.exports = {Uploader};
+module.exports = { Uploader };
